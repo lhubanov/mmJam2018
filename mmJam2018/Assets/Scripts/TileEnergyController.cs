c9f7bf1bad94306784615f3e84dbb6f0d61006dc
lhubanov <lachezar.hubanov@keysight.com>
2018-05-19 13:11:22 +0100

    prefabs dir added

---
+++ mmJam2018/Assets/Scripts/TileEnergyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileEnergyController : MonoBehaviour
+{
+    //maybe add a var keeping track of land tile energy
+    private Color startingTileColor;
+
+    void Start()
+    {
+        startingTileColor = GetComponent<Renderer>().material.color;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player") {
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player.ActionButtonPressed) {
+                GetComponentInParent<Renderer>().material.color = new Color(startingTileColor.r - 0.05f, startingTileColor.g - 0.05f, startingTileColor.b - 0.05f);
+                startingTileColor = 
+            }
+        }
+    }
+}
