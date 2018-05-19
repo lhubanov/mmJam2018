a516b8afeeeb1b59f056b945138cd1162660e7b9
lhubanov <lachezar.hubanov@keysight.com>
2018-05-19 20:00:40 +0100

    audio trigger @ mum added

---
+++ mmJam2018/Assets/Scripts/DialogueAudioHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueAudioHandler : MonoBehaviour {
+
+    public  AudioClip momLine1;
+    public  AudioClip childLine1;
+    private AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.loop = false;
+        //StartCoroutine(playDialogue());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //&& at whatever point of the story
+            StartCoroutine(playDialogue());
+        }
+    }
+
+    IEnumerator playDialogue()
+    {
+        audio.clip = momLine1;
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = childLine1;
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = momLine1;
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = childLine1;
+        audio.Play();
+    }
+}
