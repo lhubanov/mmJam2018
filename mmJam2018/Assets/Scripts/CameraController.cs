28615d1e73e34c3089d38c3f2a39fad8a49aaba7
lhubanov <lachezar.hubanov@keysight.com>
2018-09-19 17:19:12 +0100

    Moved the mom health script to StateMachine & delegated methods to player and mom

--- mmJam2018/Assets/Scripts/CameraController.cs
+++ mmJam2018/Assets/Scripts/CameraController.cs
@@ -1,19 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
-
+public class CameraController : MonoBehaviour
+{
     public GameObject player;
     private Vector3 offset;
 
-    // Use this for initialization
-    void Start () {
+    //FIXME: Add intro motion here (can just e a left-to-right motion with a vector.lerp?)
+
+
+    void Start()
+    {
         offset = transform.position - player.transform.position;
 	}
 	
-	// LateUpdate is called once per frame, but runs after all items in Update() are processed - e.g. after movement
-	void LateUpdate () {
+	void LateUpdate()
+    {
         transform.position = player.transform.position + offset;
-        //Debug.Log("Offset: " + offset);
 	}
 }
