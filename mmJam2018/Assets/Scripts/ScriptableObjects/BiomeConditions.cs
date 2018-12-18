1b0f89db0e40fa91570af42fa16326bb8dc2c8b2
lhubanov <lachezar.hubanov@keysight.com>
2018-12-18 15:47:33 +0000

    Moved elevations to settable members; moved dict initialization to SO Awake()

--- mmJam2018/Assets/Scripts/ScriptableObjects/BiomeConditions.cs
+++ mmJam2018/Assets/Scripts/ScriptableObjects/BiomeConditions.cs
@@ -8,13 +8,30 @@ namespace Assets.Scripts
     [CreateAssetMenu(menuName =  "")]
     public class BiomeConditions : ScriptableObject
     {
-        public Dictionary<int, BiomeType> Elevations = new Dictionary<int, BiomeType>()
+        // Elevations as editor-settable members
+        [SerializeField]
+        private int beachElevation; //3
+
+        [SerializeField]
+        private int grasslandElevation; //10
+
+        [SerializeField]
+        private int forestElevation; //20
+
+        [SerializeField]
+        private int swampElevation; //200
+
+        public Dictionary<int, BiomeType> Elevations;
+
+
+        public void Awake()
         {
-            { 3, BiomeType.BeachBiome},
-            { 10, BiomeType.GrasslandBiome},
-            { 20, BiomeType.ForestBiome},
-            { 200, BiomeType.SwampBiome}, // lol somehow this is the tallest biome
-        };
+            Elevations = new Dictionary<int, BiomeType>();
+            Elevations.Add(beachElevation, BiomeType.BeachBiome);
+            Elevations.Add(grasslandElevation, BiomeType.GrasslandBiome);
+            Elevations.Add(forestElevation, BiomeType.ForestBiome);
+            Elevations.Add(swampElevation, BiomeType.SwampBiome);
+        }
 
         // Note: All the probabilities can be held here btw- makes it hella easy to change on the fly
         //       But need a bit more refactoring to be carry the reference
